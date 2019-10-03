36dd3be09f838828dcd765d66dc5310ef0043853
FabioJSilva <fabiopt6@gmail.com>
2019-10-03 18:16:08 -0300

    SpotContoller

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -1,5 +1,19 @@
+const Spot = require('../models/Spot');
+
 module.exports = {
   async store(req, res) {
-    return res.json({ ok: true });
+    const { filename } = req.file;
+    const { company, techs, price } = req.body;
+    const { userId } = req.headers;
+
+    const spot = await Spot.create({
+      user: userId,
+      thumbnail: filename,
+      company,
+      price,
+      techs: techs.split(',').map((tech) => tech.trim())
+    });
+
+    return res.json(spot);
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
+const multer = require('multer');
 const routers = require('express').Router();
+const uploadConfig = require('./config/upload');
+
+
+const upload = multer(uploadConfig);
+
 const SessionController = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
-// GET, POST, PUT, DELETE
-
-// req.query = acessar query params para filtros.
-// req.params = acessar route params para edição (put, delete).
-// req.body = acessar o corpo da requisição
 
 routers.post('/sessions', SessionController.store);
-
-routers.post('/spots', SpotController.store);
+routers.post('/spots', upload.single('thumbnail'), SpotController.store);
 
 module.exports = routers;
