5b8b6c75e92063774cddb6014d791d928f97b6da
FabioJSilva <fabiopt6@gmail.com>
2019-10-06 13:01:19 -0300

    Criaçao da rota que retorna arquivos staticos

--- src/server.js
+++ src/server.js
@@ -1,5 +1,22 @@
-const app = require('./app');
+const cors = require('cors');
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const routes = require('./routes');
+const env = require('../.env.js');
 
-app.listen('3001', () => {
-  console.log('server listening port 3001');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
+app.use(routes);
+
+mongoose.connect(env.dbConnect, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.listen('3333', () => {
+  console.log('server listening port 3333');
 });
